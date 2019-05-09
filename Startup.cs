fcfc9ccd25ffafd18890c5f6fac4f680054381a9
Raghulgkl <50103461+Raghulgkl@users.noreply.github.com>
2019-05-09 10:35:00 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_TEST.Startup))]
+namespace mvc_TEST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
